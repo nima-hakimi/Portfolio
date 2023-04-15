33e63abc2a61ac391d417fe30cac526177da4827
Nima Hakimi <hakimi.nima1@gmail.com>
2023-04-15 18:29:03 +0200

    one more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
-// pages/_app.tsx
+// src/pages/_app.tsx
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
